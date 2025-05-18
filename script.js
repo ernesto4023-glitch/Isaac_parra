6e2846d4c13a7241c33a7b4a02816fd6a20c0cbd
ernesto4023-glitch <ernesto.dpa28@gmail.com>
2025-05-18 14:32:42 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+    const swiper = new Swiper(".swiper", {
+        loop: true,
+        autoplay: {
+        delay: 2500, // tiempo en milisegundos (2.5 segundos)
+        disableOnInteraction: false,
+        },
+    });
